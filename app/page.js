87d4ec07518117302641abb78cdee175ea9f792c
devdashk66 <devdashk66@gmail.com>
2025-07-04 17:09:13 +0600

    add construction

--- app/page.js
+++ app/page.js
@@ -1,59 +1,15 @@
 export default function Home() {
   return (
-    <div className="p-10">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde illo, ipsa
-      provident adipisci aspernatur sint optio deserunt libero sit quasi
-      repellat, et, doloremque ipsum maiores. Odit aspernatur beatae libero
-      atque natus, molestias nihil suscipit sint adipisci minima reiciendis, a
-      hic autem dolorum. Quas aperiam ipsa deleniti quo blanditiis dignissimos
-      reprehenderit tenetur cupiditate, fugiat numquam nobis magni sequi cum
-      quos asperiores odit earum dicta maxime sapiente corrupti animi iste
-      consequuntur quis inventore. Nulla laudantium odio fugiat quas, sunt ullam
-      et quaerat possimus deleniti distinctio hic. Provident, iste enim nihil
-      fugiat earum minima explicabo laudantium neque quibusdam maiores ducimus,
-      vitae maxime vel aperiam, reprehenderit possimus? Nobis rerum maxime
-      tempore vero architecto quibusdam sunt, dolore nam earum praesentium
-      distinctio, porro quasi consequatur veritatis quam impedit molestiae
-      ipsam, magnam perferendis tempora consectetur labore dignissimos dicta
-      numquam! Veniam dolore exercitationem blanditiis eius dignissimos
-      inventore sit et doloribus cupiditate dolores aspernatur aperiam odit
-      aliquid beatae laborum, libero qui quae sed minima labore. Neque inventore
-      maiores culpa saepe laboriosam odio totam eveniet labore, explicabo
-      facilis? Quasi, aut! Adipisci deleniti, doloremque excepturi ex totam odit
-      aut similique at debitis, magnam suscipit mollitia laudantium maxime
-      minima molestiae! Voluptatibus repellat asperiores, beatae eveniet
-      perspiciatis cupiditate quasi maiores quisquam ipsa! Sequi error ab
-      perspiciatis facilis vitae quae voluptatibus dicta rerum aut placeat iste,
-      minima qui doloremque ea nam beatae repudiandae obcaecati, eaque earum
-      facere? Perferendis nulla necessitatibus id laudantium, illo recusandae
-      nemo cupiditate ut. Ducimus doloremque facere nisi eius rerum eum quod
-      beatae sequi. Ducimus nostrum accusantium, perspiciatis, sunt quia, eius
-      dicta hic nisi nobis aliquid non atque neque tempora. Amet cum quaerat
-      inventore officia veniam provident dicta iusto atque nisi harum reiciendis
-      ex perspiciatis velit, fuga magni modi blanditiis odio eligendi esse
-      voluptatum? Fugit molestiae commodi illum soluta nam ab deserunt id,
-      cupiditate ea, sequi iure atque suscipit praesentium nemo odio? Veritatis
-      repudiandae sint amet cum exercitationem aliquid quibusdam fugit impedit,
-      sunt non tempore facere nam eos hic laudantium sequi ipsa delectus
-      pariatur magnam quis labore illum aperiam nobis. Quia veritatis corrupti
-      saepe laboriosam laudantium illo sint facere aliquid voluptates est
-      consequatur vitae praesentium, sequi, quasi id pariatur optio labore non
-      dignissimos dolor quo ducimus aut eligendi tempore! Eveniet quasi quae
-      libero, beatae omnis repudiandae voluptatibus quia. Ad esse harum quisquam
-      repellendus dolor perspiciatis sit iure ducimus. Voluptates quis, quam
-      dignissimos eum asperiores praesentium facilis! Fugiat adipisci facere
-      debitis reprehenderit quam cumque labore voluptate expedita? Quis nostrum
-      illum dicta ex commodi, nesciunt modi tempora iste expedita porro,
-      molestias sapiente cumque optio nobis aut fugiat vel explicabo aliquid.
-      Qui sit ullam saepe! In corporis temporibus modi quos ipsum eveniet alias
-      dolorem mollitia maiores itaque consequuntur sapiente labore possimus
-      libero minus cumque fugit distinctio qui eligendi, soluta sunt. Neque
-      officia corporis praesentium asperiores, assumenda cupiditate iste,
-      doloribus fugiat enim hic quia facere itaque odio aliquid architecto rem
-      saepe unde facilis est quis fugit modi eius explicabo. Sint quasi maiores
-      ducimus, aut debitis modi quo repellat iste exercitationem, sunt illum
-      consectetur dolore quod, eveniet enim illo accusantium suscipit? Cumque
-      modi iste aliquid perferendis!
-    </div>
+    <section>
+      {/* make a under constraciton page using tailwind css */}
+      <div className="flex items-center justify-center min-h-screen ">
+        <div className="text-center">
+          <h1 className="text-4xl font-bold  mb-4">Under Construction</h1>
+          <p className="text-lg mb-8">
+            We are working hard to get this page ready. Stay tuned!
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
